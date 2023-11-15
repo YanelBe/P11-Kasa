c703c430bda2b4834b7ba45228cace88dd55e549
Yanel <yanel.bensa@gmail.com>
2023-11-15 15:16:34 +0100

    Ajout de NavLink pour les liens actifs

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 //Le composant Nav sert à rediriger vers les liens correspondants du site
 export default function Nav() {
+		
 	return (
 		<nav className="nav-header">
-			<Link to="/" className="nav-header__link-home">
+			<NavLink to="/" className="nav-header__link-home">
 				Accueil
-			</Link>
-			<Link to="/about" className="nav-header__link-about">
+			</NavLink>
+			<NavLink to="/about" className="nav-header__link-about">
 				A Propos
-			</Link>
+			</NavLink>
 		</nav>
 	);
 }
--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -5,27 +5,27 @@ import right from "../../assets/images/arrow_forward.svg";
 export default function Slideshow({ slides }) {
 
 	//On utilise le hook useState pour mettre à jour l'état des slides, par défaut à 0
-	const [current, setCurrent] = useState(0); //je définis l'index du premier slide à 0
+	const [current, setCurrent] = useState(0);
 
 	//
-	const length = slides ? slides.length : 0; // longueur du tableau de slides
+	const length = slides ? slides.length : 0;
 
 	//Fonction pour passer à la slide suivante
 	//L'opérateur ternaire permet de repasser à la première image lorsqu'on clique sur la flèche avant sur la dernière image
 	function nextSlide() {
-		setCurrent(current === length - 1 ? 0 : current + 1); // on repart au premier slide quand on arrive au dernier
+		setCurrent(current === length - 1 ? 0 : current + 1);
 	};
 
 	//Fonction pour passer à la slide précédente
 	//L'opérateur ternaire permet de repasser à la dernière image lorsqu'on clique sur la flèche arrière sur la première image
 	function previousSlide() {
-		setCurrent(current === 0 ? length - 1 : current - 1); // on repart au dernier slide quand on est au premier
+		setCurrent(current === 0 ? length - 1 : current - 1);
 	};
 
 
 	return (
 		<section id="slideshow-container">
-			<div class="arrow-container">
+			<div className="arrow-container">
 				{/*Les flèches sont affichées uniquement si la longueur des slides est supérieure à 1*/}
 				{length > 1 && (
 					<img
